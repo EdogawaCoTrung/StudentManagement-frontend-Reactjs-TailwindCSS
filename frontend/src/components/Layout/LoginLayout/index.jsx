653f390e533f31c4c75236f17f142bcb0d195725
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-03-31 13:53:17 +0700

    sua man login

--- frontend/src/components/Layout/LoginLayout/index.jsx
+++ frontend/src/components/Layout/LoginLayout/index.jsx
@@ -1,3 +1,4 @@
+// import * as React from   'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -5,16 +6,17 @@ import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import {MainTheme} from '../../../assets/Theme'
-import {ThemeProvider} from '@mui/material/styles'
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { ThemeProvider } from '@mui/material/styles';
+import { MainTheme } from '../../../assets/Theme';
 
 const defaultTheme = MainTheme;
 
-export default function LogIn() {
+export default function SignIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -24,81 +26,85 @@ export default function LogIn() {
     });
   };
 
+  const divStyle = {
+    backgroundImage: `url(public/Login.png)`,
+    backgroundSize: 'cover',
+    height: '100%',
+    width: '100%',
+    padding: '5%'
+  };
+
+
+
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+    <ThemeProvider theme={defaultTheme}
+    >
+      <div className="bg-cover" style={divStyle}>
+      <Container className="blurred-container box-sizing: content-box;" component="main" maxWidth="xs" 
+          style ={{
+            padding: '20px',
+            backdropFilter: 'blur(20px)',
+            borderRadius: '10px',
+          }}>
         <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={6}
+        <Box
           sx={{
-            backgroundImage: 'url(/Login.png)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'fit',
-            backgroundPosition: 'left',
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
-        />
-        <Grid item sx={{paddingTop: 10}} xs={12} sm={8} md={6} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{width: 100, height:100, m: 1, bgcolor: 'secondary.main' }}>
-              <PersonOutlineIcon />
-            </Avatar>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1, paddingLeft: 10, paddingRight: 10 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="User Name"
-                name="username"
-                autoComplete="username"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Đăng Nhập
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Tên đăng nhập"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Mật khẩu"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Nhớ tôi"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Đăng Nhập
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Quên mật khẩu?
+                </Link>
               </Grid>
-            </Box>
+            </Grid>
           </Box>
-        </Grid>
-      </Grid>
+        </Box>
+      </Container>
+      </div>
     </ThemeProvider>
   );
 }
