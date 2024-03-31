a2fbd98f590d0dbe4b81d8e614aaac7ff5700deb
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-03-31 21:14:36 +0700

    them sidebar

--- frontend/src/components/Layout/DefaultLayout/index.jsx
+++ frontend/src/components/Layout/DefaultLayout/index.jsx
@@ -1,18 +1,15 @@
 
 import SideBar from "../../share/SideBar"
-import MyHeader from "../../share/MyHeader";
 import PropTypes from 'prop-types';
 
 
-export default function DefaultLayout({ children }) {
+export default function DefaultLayout() {
   return (
     <div>
       <div className="">
         <div className="flex w-[1150px] max-w-full p-1">
           <SideBar />
-          <div className="ml-6 flex-1">{children}</div>
-          <MyHeader/>
-        </div>
+          </div>
       </div>
     </div>
   )
