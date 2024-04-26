6e8a117f1ee275d743b5d57e409d99cd262feff1
EdogawaCoTrung <quangtrungproki@gmail.com>
2024-04-26 12:54:17 +0700

    add

--- frontend/src/components/share/Card/index.jsx
+++ frontend/src/components/share/Card/index.jsx
@@ -1,13 +1,12 @@
-import * as React from "react"
 import PropTypes from "prop-types"
 import ButtonAndIcon from "../ButtonAndIcon"
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid"
 
-function Card({className, teacherName }) {
+function Card({ className, teacherName }) {
   return (
     <Grid container direction="column">
       <Grid item>
-        <ButtonAndIcon btnName={className}/>
+        <ButtonAndIcon btnName={className} />
       </Grid>
       <Grid item>
         <span className=" font-bold">{teacherName}</span>
