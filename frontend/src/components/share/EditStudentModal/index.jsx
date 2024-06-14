6bcf06d86c8efc0065e6f6d3ad39c5a1149eaf87
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-06-14 12:22:16 +0700

    fix date

--- frontend/src/components/share/EditStudentModal/index.jsx
+++ frontend/src/components/share/EditStudentModal/index.jsx
@@ -13,25 +13,22 @@ import { Dialog, Transition } from "@headlessui/react"
 import { Fragment, useState } from "react"
 import { accountApi} from "../../../apis"
 import Button from '@mui/material/Button';
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker"
+import dayjs from "dayjs"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 
 export default function EditStudent ({ isOpenEditStudent, closeEditStudent, id }) {
 
-    const formatDate = (dateString) => {
-        const date = new Date(dateString)
-        const day = date.getDate()
-        const month = date.getMonth() + 1
-        const year = date.getFullYear()
-        return `${day}/${month}/${year}`
-    }
     const [className, setClass] = React.useState("10A2")
     const [grade, setGrade] = React.useState("10")
     const [student, setStudent] = React.useState({})
     const [user, setUser] = React.useState({})
     const [name, setName] = React.useState("")
-    const [birthDate, setBirthDate] = React.useState("")
+    const [birthDate, setBirthDate] = React.useState(dayjs("2022-04-17"))
     const [gender, setGender] = React.useState("")
-    const [startDate, setStartDate] = React.useState("")
+    const [startDate, setStartDate] = React.useState(dayjs("2022-04-17"))
     const [username, setUsername] = React.useState("")
     const [email, setEmail] = React.useState("")
     const [address, setAddress] = React.useState("")
@@ -67,24 +64,12 @@ export default function EditStudent ({ isOpenEditStudent, closeEditStudent, id }
         }
     }, [student]);
 
-    function setValues() {
-        setName(`${student.studentname}`);
-        setBirthDate(formatDate(student.birthDate));
-        if (student.gender === 0) setGender("Nam")
-        else setGender("Nữ")
-        setStartDate(formatDate(student.startDate));
-        setEmail(`${user.email}`);
-        setAddress(student.address);
-        setStatusInYear(student.statusInYear);
-        setUsername(user.username);
-    }
-
     React.useEffect(() => {
         if (student && user) {
           setName(student.studentname || "");
-          setBirthDate(formatDate(student.birthDate) || "");
+          setBirthDate(dayjs(student.birthDate) || "");
           setGender(student.gender === 0 ? "Nam" : "Nữ");
-          setStartDate(formatDate(student.startDate) || "");
+          setStartDate(dayjs(student.startDate) || "");
           setEmail(user.email || "");
           setAddress(student.address || "");
           setStatusInYear(student.statusInYear || 0);
@@ -166,39 +151,43 @@ export default function EditStudent ({ isOpenEditStudent, closeEditStudent, id }
                                         </div>
                                         </div>
                                         <div>
-                                            <div className="flex flex-row">
-                                                <TextField
-                                                    label="Ngày sinh"
-                                                    value={birthDate}
-                                                    onChange={(event) => setBirthDate(event.target.value)}
-                                                    className="pl-10"
-                                                    style={{ fontSize: "18px", marginTop: "40px", marginLeft: "40px" }}
+                                            <div className="flex flex-row mt-8 ml-2">
+                                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                                <DatePicker
+                                                sx={{ marginLeft: 4, width: "11vw" }}
+                                                label="Ngày sinh"
+                                                value={birthDate}
+                                                onChange={(newValue) => setBirthDate(newValue)}
+                                                defaultValue={dayjs("2022-04-17")}
                                                 />
+                                            </LocalizationProvider>
                                                 <div className="ml-40">
                                                     <TextField
                                                         label="Giới tính"
                                                         value={gender}
                                                         onChange={(event) => setGender(event.target.value)}
                                                         className="pl-10"
-                                                        style={{ fontSize: "18px", marginTop: "36px", marginRight: "20px", marginLeft: "-40px" }}
+                                                        style={{ fontSize: "18px", marginRight: "20px" }}
                                                     />
                                                 </div>
                                             </div>
                                             <div>
-                                                <div className = "flex flex-row">
-                                                <TextField
+                                                <div className = "flex flex-row mt-6 ml-2">
+                                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                                    <DatePicker
+                                                    sx={{ marginLeft: 4, width: "11vw" }}
                                                     label="Ngày nhập học"
                                                     value={startDate}
-                                                    onChange={(event) => setStartDate(event.target.value)}
-                                                    className="pl-10"
-                                                    style={{ fontSize: "18px", marginTop: "20px", marginLeft: "40px" }}
-                                                />
+                                                    onChange={(newValue) => setStartDate(newValue)}
+                                                    defaultValue={dayjs("2022-04-17")}
+                                                    />
+                                                </LocalizationProvider>
                                                 <TextField
                                                     label="Khối"
                                                     value={grade}
                                                     onChange={(event) => setGrade(event.target.value)}
                                                     className="pl-10"
-                                                    style={{ fontSize: "18px", marginLeft: "120px", marginTop: "18px" }}
+                                                    style={{ fontSize: "18px", marginLeft: "160px"}}
                                                 />
                                                 </div>
                                                 <div>
@@ -214,7 +203,7 @@ export default function EditStudent ({ isOpenEditStudent, closeEditStudent, id }
                                                     value={email}
                                                     onChange={(event) => setEmail(event.target.value)}
                                                     className="pl-10"
-                                                    style={{ fontSize: "18px", marginTop: "20px", marginLeft: "120px" }}
+                                                    style={{ fontSize: "18px", marginTop: "20px", marginLeft: "106px" }}
                                                 />
                                                 </div>
                                                 <TextField
@@ -229,7 +218,7 @@ export default function EditStudent ({ isOpenEditStudent, closeEditStudent, id }
                                                     value={statusInYear === 0 ? "Chưa hoàn thành" : "Đã hoàn thành"}
                                                     onChange={(event) => setStatusInYear(event.target.value)}
                                                     className="pl-10"
-                                                    style={{ fontSize: "18px", marginTop: "20px", marginLeft: "120px" }}
+                                                    style={{ fontSize: "18px", marginTop: "20px", marginLeft: "106px" }}
                                                 />
                                             </div>
                                         </div>
